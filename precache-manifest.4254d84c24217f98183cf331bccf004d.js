4dbd8861dcd84bf5c6d4500dbfba863603288307
Lukasz <syljon2@gmail.com>
2019-05-04 17:16:59 +0200

    Updates

---
+++ precache-manifest.4254d84c24217f98183cf331bccf004d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da364c0c0bf54a9934181f6b438b056",
+    "url": "/skygate/index.html"
+  },
+  {
+    "revision": "56eaa208129e6e995d9e",
+    "url": "/skygate/static/css/main.4d7ef9f5.chunk.css"
+  },
+  {
+    "revision": "873f6d5082ac9b22a9a2",
+    "url": "/skygate/static/js/2.853cc5a0.chunk.js"
+  },
+  {
+    "revision": "56eaa208129e6e995d9e",
+    "url": "/skygate/static/js/main.7125dc6f.chunk.js"
+  },
+  {
+    "revision": "c6610ea0422687f844f5",
+    "url": "/skygate/static/js/runtime~main.10884107.js"
+  }
+]);
